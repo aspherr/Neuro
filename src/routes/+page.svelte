02264201a0190dce9aadb1e186269d78f0b093b7
aspherr <huzaifa.syedd@gmail.com>
2025-03-11 23:06:07 +0000

    imported tailwindcss and added a logo

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+
+</script>
+
+<main class="h-screen w-screen bg-zinc-900 text-white">
+  <div class="container">
+    <img src="/logo.png" alt="Neuro Logo" class="pt-6 w-42 h-48 mx-auto"/>
+  </div>
+</main>
