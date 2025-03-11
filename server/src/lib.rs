051c1c44310b96d30a32a91253c9da4c7032d443
aspherr <huzaifa.syedd@gmail.com>
2025-03-11 23:59:45 +0000

    added title+version no. & home page buttons

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,14 +1,27 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use log::info;
+use simple_logger;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_app_version(app_handle: tauri::AppHandle) -> String {
+    app_handle
+    .package_info()
+    .version
+    .to_string()
+}
+
+#[tauri::command]
+fn logger(message: String) {
+    info!("Log: {}", message);
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    simple_logger::init()
+    .unwrap();
+    
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_app_version, logger])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,50 @@
 <script lang="ts">
+  import { onMount } from 'svelte';
+  import { invoke } from '@tauri-apps/api/core';
 
+  let appVersion = 'loading...';
+  onMount(async() => {
+    appVersion = await invoke ('get_app_version');
+  });
+
+  function createVault() {
+    const message = "vault created";
+    invoke("logger", { message });
+  }
+
+  function openVault() {
+    const message = "vault opened";
+    invoke("logger", { message });
+  }
+
+  function openSyncedVault() {
+    const message = "synced vault opened";
+    invoke("logger", { message });
+  }
 </script>
 
+
 <main class="h-screen w-screen bg-zinc-900 text-white">
-  <div class="container">
-    <img src="/logo.png" alt="Neuro Logo" class="pt-6 w-42 h-48 mx-auto"/>
+  <div class="">
+    <img src="/logo.png" alt="Neuro Logo" class="w-42 h-48 mx-auto pt-6"/>
+  </div>
+
+  <div class="text-center">
+    <h1 class="text-5xl font-bold -mt-7">Neuro</h1>
+    <p class="text-base text-gray-400">Version: {appVersion}</p>
+  </div>
+
+  <div class="mt-10 w-full flex flex-col items-center">
+    <div class="w-64 flex flex-col space-y-3">
+      <button class="w-full py-3 bg-orange-600 text-white rounded-lg hover:bg-orange-700 transition" onclick={createVault}>
+        Create Vault
+      </button>
+      <button class="w-full py-3 bg-gray-700 text-white rounded-lg hover:bg-gray-800 transition" onclick={openVault}>
+        Open Vault
+      </button>
+      <button class="w-full py-3 bg-gray-700 text-white rounded-lg hover:bg-gray-800 transition" onclick={openSyncedVault}>
+        Open Synced Vault
+      </button>
+    </div>
   </div>
 </main>
