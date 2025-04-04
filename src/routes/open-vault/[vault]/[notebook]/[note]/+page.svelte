9ae058d758d2b233d2b082fdf475666b96ea9b4d
aspherr <huzaifa.syedd@gmail.com>
2025-04-04 19:23:14 +0100

    implemented markdown formatting

--- src/routes/open-vault/[vault]/[notebook]/[note]/+page.svelte
+++ src/routes/open-vault/[vault]/[notebook]/[note]/+page.svelte
@@ -3,4 +3,6 @@
 	let someMarkdown = "# Hello from Markdown! \n This is _really_ just a markdown string! \n\n Using `marked` we can generate a HTML string from it and using Sveltes `@html` tag we can render that HTML easily!"
 </script>
 
-{@html marked(someMarkdown)}
+<div class="prose">
+    {@html marked(someMarkdown)}
+</div>
